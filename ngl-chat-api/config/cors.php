27aa82d063d907a11e5e2272ea66fe88a71e4635
jctech111 <jctech333@gmail.com>
2025-05-17 15:22:33 +0100

    worked on the Cors error

---
+++ ngl-chat-api/config/cors.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['http://localhost:5173'], // Replace with your React app URL
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true, // Important if using cookies/session
+
+];
